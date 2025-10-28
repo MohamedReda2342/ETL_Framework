ff2b92cc1ceee98ea6b69cca2e7795f8cc3a4933
Mohamed Reda <78495546+MohamedReda2342@users.noreply.github.com>
2025-10-28 12:00:49 +0300

    Update generating_scripts.py

--- generating_scripts.py
+++ generating_scripts.py
@@ -11,7 +11,7 @@ import sys
 from colorama import init, Fore, Back, Style
 init(autoreset=True)  # Initialize colorama
 from itertools import chain  
-import regex as re
+# import regex as re
 import math
 from decimal import Decimal
 
